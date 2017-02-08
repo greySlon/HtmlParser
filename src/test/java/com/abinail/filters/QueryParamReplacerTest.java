0d7bb9ee162111335af865530708b4102f5511bd
GreySlon <garanenkosergii@gmail.com>
2017-02-08 02:14:56 +0200

    hz

---
+++ src/test/java/com/abinail/filters/QueryParamReplacerTest.java
@@ -0,0 +1,14 @@
+package com.abinail.filters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Sergii on 08.02.2017.
+ */
+class QueryParamReplacerTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+}
